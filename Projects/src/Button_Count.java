131418299a1daaa071b46abcddef04b05a2a327b
TboyBell <bellnewtech@gmail.com>
2024-08-28 09:35:47 +0100

    Button_Count
    *First Commit*

---
+++ Projects/src/Button_Count.java
@@ -0,0 +1,66 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+public class Button_Count implements ActionListener {
+
+	int count;
+	JButton button = new JButton("Click Me");
+	JLabel label = new JLabel("Number Of Clicks: 0");
+	JPanel panel = new JPanel();
+	JFrame frame;
+	
+	
+	public Button_Count() {
+		frame = new JFrame("Button-Count");
+		frame.setSize(300, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+		
+		button.setFocusable(false);
+		button.setToolTipText("Press To Increment Count");
+		button.addActionListener(this);
+		
+		
+		
+		
+		
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
+		
+		
+		
+		
+				
+		
+		frame.add(panel);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if(e.getSource() == button) {
+			count++;
+			label.setText("Number Of Clicks: "+ count);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		new Button_Count();
+	}
+	
+}
